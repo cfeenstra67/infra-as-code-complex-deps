2b28bec423e2e82c0f7cf9de3310dc8168d1d780
cfeenstra67 <cameron.l.feenstra@gmail.com>
2021-04-10 21:30:14 -0400

    update graphs

--- statey/statey_module.py
+++ statey/statey_module.py
@@ -9,9 +9,9 @@ def resources(session, public_key: str):
         publicKey=public_key,
     )
 
-    security_group_2 = aws.ec2.SecurityGroup(
+    security_group = aws.ec2.SecurityGroup(
         namePrefix="test_sg",
-        description="My security group. 2",
+        description="My security group.",
         ingress=[
             {
                 "protocol": "tcp",
@@ -29,8 +29,7 @@ def resources(session, public_key: str):
         ami="ami-07d02ee1eeb0c996c",
         instanceType="t2.micro",
         keyName=key.keyName,
-        vpcSecurityGroupIds=[security_group_2.id],
-        # userData="#"
+        vpcSecurityGroupIds=[security_group.id],
     )
 
 
